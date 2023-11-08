2785987f94a4101ec6adc4576a649ead53bc902c
darleet <darleet@mail.ru>
2023-11-09 02:13:34 +0300

    Moved authentication service for lab4 server to separate file

--- lab4/server/main.go
+++ lab4/server/main.go
@@ -12,18 +12,6 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 )
 
-// auth represents authentication service
-type auth struct {
-	// users stores [login]password pairs
-	users map[string]string
-}
-
-// handlePassword handles password authentication for ssh
-func (a *auth) handlePassword(ctx ssh.Context, password string) bool {
-	pass, exists := a.users[ctx.User()]
-	return exists && pass == password
-}
-
 // handleSFTP handles SFTP connection (за это обещали доп балл!!)
 func handleSFTP(sess ssh.Session) {
 	server, err := sftp.NewServer(sess)
